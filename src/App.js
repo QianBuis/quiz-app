b697005743ec0523d635e514bb3122debe0e59b9
Qian Buis <Qian.Buis@student.rocva.nl>
2025-01-22 10:25:07 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+import React from 'react';
+
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+    this.getNumbers();
+  }
+  
+
+  
+  render() {
+    return (
+      <div className="app">
+
+      </div>
+    );
+  }
+
+}
+
+export default App;
